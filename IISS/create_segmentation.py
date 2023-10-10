d241112a9240687398664871809010263b84dcc4
franchesoni <marchesoniacland@gmail.com>
2023-10-10 09:06:49 +0200

    use np + rename

--- IISS/create_segmentation.py
+++ IISS/create_segmentation.py
@@ -1,20 +1,20 @@
-import torch
+import numpy as np
 
 def create_segmentation(masks_per_image, labels, clicks):
     # write classified labels to pred image
-    frame_preds = []
+    pred_masks = []
     ind = 0
     for masks in masks_per_image:
-        frame_pred = torch.zeros_like(masks[0]['segmentation'])
+        frame_pred = np.zeros_like(masks[0]['segmentation'], dtype=int)
         for mask in masks:
             frame_pred += mask['segmentation'] * labels[ind]
             ind += 1
-        frame_preds.append(frame_pred)
+        pred_masks.append(frame_pred)
 
     # overwrite frame_preds from clicks
     for click in clicks:
         frame_ind, mask_ind, is_pos = click
         clicked_region = masks_per_image[frame_ind][mask_ind]['segmentation']
         pred_masks[frame_ind] =  pred_masks[frame_ind] * (~clicked_region) + clicked_region * (2*is_pos - 1)
-    pred_masks = [fp > 0 for fp in frame_preds]  # thresold the labels
+    pred_masks = [fp > 0 for fp in pred_masks]  # thresold the labels
     return pred_masks
