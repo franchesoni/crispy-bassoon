7179fea5e96fb4605b185e0b66527bea8c767c5a
franchesoni <marchesoniacland@gmail.com>
2023-11-09 15:13:44 +0100

    add zerowaste and suim

--- mess/experiments/iis/precompute_masks.py
+++ mess/experiments/iis/precompute_masks.py
@@ -107,13 +107,17 @@ def main(mode, dev=False, ds=None):
     ds_names = get_detectron2_datasets()
 
     DATASETS=[
-           # 'atlantis_sem_seg_test', 'chase_db1_sem_seg_test', 'corrosion_cs_sem_seg_test', 'cryonuseg_sem_seg_test', 'cub_200_sem_seg_test', 'cwfid_sem_seg_test', 'dark_zurich_sem_seg_val', 'deepcrack_sem_seg_test', 'dram_sem_seg_test', 
             'foodseg103_sem_seg_test', 
             'foodseg103_sem_seg_train', 
-           # 'isaid_sem_seg_val', 'kvasir_instrument_sem_seg_test', 
             'mhp_v1_sem_seg_test',
             'mhp_v1_sem_seg_train',
-           # 'paxray_sem_seg_test_bones', 'paxray_sem_seg_test_diaphragm', 'paxray_sem_seg_test_lungs', 'paxray_sem_seg_test_mediastinum', 'pst900_sem_seg_test', 'suim_sem_seg_test', 'worldfloods_sem_seg_test_irrg', 'zerowaste_sem_seg_test', 'ndd20_sem_seg_test', 'mypascalvoc_sem_seg_test', 'mysbd_sem_seg_test', 'mygrabcut_sem_seg_test'
+            'suim_sem_seg_test', 
+            'suim_sem_seg_train', 
+            'zerowaste_sem_seg_test', 
+            'zerowaste_sem_seg_train', 
+           # 'atlantis_sem_seg_test', 'chase_db1_sem_seg_test', 'corrosion_cs_sem_seg_test', 'cryonuseg_sem_seg_test', 'cub_200_sem_seg_test', 'cwfid_sem_seg_test', 'dark_zurich_sem_seg_val', 'deepcrack_sem_seg_test', 'dram_sem_seg_test', 
+           # 'isaid_sem_seg_val', 'kvasir_instrument_sem_seg_test', 
+           # 'paxray_sem_seg_test_bones', 'paxray_sem_seg_test_diaphragm', 'paxray_sem_seg_test_lungs', 'paxray_sem_seg_test_mediastinum', 'pst900_sem_seg_test', 'worldfloods_sem_seg_test_irrg', 'ndd20_sem_seg_test', 'mypascalvoc_sem_seg_test', 'mysbd_sem_seg_test', 'mygrabcut_sem_seg_test'
            ]
     assert (ds is None) or ds in DATASETS 
     DATASETS = DATASETS if ds is None else [ds]
